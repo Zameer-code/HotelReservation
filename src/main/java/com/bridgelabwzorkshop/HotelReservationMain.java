70d5c8ab34deffd3bddb97fe4a31d07a35e412b0
Zameer-code <84001846+Zameer-code@users.noreply.github.com>
2021-06-26 09:19:29 +0530

    Add files via upload

---
+++ src/main/java/com/bridgelabwzorkshop/HotelReservationMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabwzorkshop;
+
+public class HotelReservationMain {
+	 public static void main(String[] args) {
+	        HotelReservation hotelBuilder = new HotelReservation();
+	        hotelBuilder.viewHotels();
+	    }
+
+}
